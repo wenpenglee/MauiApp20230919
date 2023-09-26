fdb610e41d52cbb475c3db669b319c59da6db92f
wenpenglee <leewenpengl@gmail.com>
2023-09-26 15:11:31 +0800

    Add FruitPage

---
+++ MauiApp20230919/FruitPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp20230919;
+
+public partial class FruitPage : ContentPage
+{
+    public FruitPage()
+    {
+        InitializeComponent();
+    }
+
+    private void FruitDetailButton_OnClicked(object sender, EventArgs e)
+    {
+        Navigation.PushModalAsync(new FruitDetailPage());
+
+    }
+}
